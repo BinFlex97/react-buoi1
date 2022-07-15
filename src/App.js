d670e36d304f1c801d879895aaf2d4559939d93a
Khanh197 <khanhquangdinh197@gmail.com>
2022-07-15 18:05:48 +0700

    code

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './BaiTapThucHanhLayout/Header';
+import Banner from './BaiTapThucHanhLayout/Banner';
+import Item from './BaiTapThucHanhLayout/Item';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Banner/>
+      <Item/>
+    </div>
+  );
+}
+
+export default App;
